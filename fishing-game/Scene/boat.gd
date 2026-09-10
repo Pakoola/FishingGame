c92e05c710eb967fabd9b0f6e547ec9275754c12
Justin <jpakul14@gmail.com>
2026-09-10 18:03:02 -0500

    Fisherman and Boat Animations and Interactions

--- fishing-game/Scene/boat.gd
+++ fishing-game/Scene/boat.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+const FISHERMAN = preload("uid://c6c4k62ct1gn0")
+
+
 const GRAVITY = 200.0
 
 func _physics_process(delta):
@@ -9,7 +12,6 @@ func _physics_process(delta):
 	move_and_collide(motion)
 
 
-func _on_enter_area_area_entered(area: Area2D) -> void:
-	# TODO: Trying to build this on my own but how do I get the guy to be on tied to the boat
-	# and how do I make the boat drive?
-	print("Press E to enter boat")
+func _on_enter_area_body_entered(body: Node2D) -> void:
+	print("Press E to enter...")
+	pass
--- fishing-game/Scene/fisherman.gd
+++ fishing-game/Scene/fisherman.gd
@@ -17,12 +17,12 @@ func _physics_process(delta):
 		velocity.x = direction * speed
 		
 		if velocity.x != 0:
-			%AnimatedSprite2D.play("walk")
+			%Animation.play("walk")
 			if velocity.x < 0:
-				%AnimatedSprite2D.flip_h = true
+				%Animation.flip_h = true
 			else:
-				%AnimatedSprite2D.flip_h = false
+				%Animation.flip_h = false
 		else:
-			%AnimatedSprite2D.play("idle")
+			%Animation.play("idle")
 			
 		move_and_slide()
