0e2abd0f83b8b968ef8b7ef32f6b595915991c01
Justin <jpakul14@gmail.com>
2026-09-17 17:38:10 -0500

    Added fishing difficulty
    - Slider
    - Difficulty
    - UI for success

--- fishing-game/Scripts/fishing_minigame.gd
+++ fishing-game/Scripts/fishing_minigame.gd
@@ -1,36 +1,59 @@
 extends Node2D
 
+
+@export_enum("EASY", "MEDIUM", "HARD", "VERY_HARD", "LEGENDARY") var fishDifficultyProp: String
+
 @onready var v_slider: VSlider = $ColorRect/VSlider
 @onready var hook_timer: Timer = $HookTimer
 @onready var color_rect: ColorRect = $ColorRect
 
-enum STATE {FISHING, NOT_FISHING}
-
 var fishStrength: int
-var state: STATE = STATE.FISHING
 var fishingCaught: bool = false
+var fish_difficulty = {
+	"EASY": 1,
+	"MEDIUM": .75,
+	"HARD": .5,
+	"VERY_HARD": .25,
+	"LEGENDARY": .10,
+}
+
 
 func _ready() -> void:
+	var fishDifficulty = fish_difficulty[fishDifficultyProp]
 	fishStrength = randi_range(1, 100)
-	hook_timer.start(fishStrength * .03)
-	print(fishStrength)
+	hook_timer.start(fishStrength * .05 * fishDifficulty) 
+	print(hook_timer.time_left)
+	#hook_timer.start(fishStrength * 3)
 	
 func _process(delta: float) -> void:
-	match state:
-		STATE.FISHING:
-			if v_slider.value == fishStrength and !fishingCaught:
-				fishingCaught = true
-				$"%Fish Outcome".text = "Fish Caught!"
-				%"Fish Outcome".visible = true
-				hook_timer.stop()
-				color_rect.visible = false
-		STATE.NOT_FISHING:
-			fishingCaught = false
-			$"%Fish Outcome".text = "Fish escaped..."
-			%"Fish Outcome".visible = true
-			color_rect.visible = false
-			#queue_free()
+	pass
 
 
 func _on_hook_timer_timeout() -> void:
-	state = STATE.NOT_FISHING
+	print_debug("Fish Strength = " + str(fishStrength) + " | Slider: " + str(v_slider.value))
+	check_catch()
+	
+func check_catch() -> void:
+	if v_slider.value == fishStrength and !fishingCaught and hook_timer.is_stopped():
+		fishingCaught = true
+		$"%Fish Outcome".text = "Fish Caught!"
+		%"Fish Outcome".visible = true
+		hook_timer.stop()
+		color_rect.visible = false
+	else:
+		fishingCaught = false
+		$"%Fish Outcome".text = "Fish escaped..."
+		%"Fish Outcome".visible = true
+		color_rect.visible = false
+		#queue_free()
+
+func _on_v_slider_value_changed(value: float) -> void:
+	value = int(value)
+	if fishStrength == value:
+		color_rect.color = Color(0.152, 0.984, 0.69, 1.0)
+	elif value <= fishStrength + 5 and value >= fishStrength - 5:
+		color_rect.color = Color(0.058, 0.524, 0.36, 1.0)
+	elif value <= fishStrength + 10 and value >= fishStrength - 10:
+		color_rect.color = Color(0.886, 0.679, 0.0, 1.0)
+	else:
+		color_rect.color = Color(0.788, 0.098, 0.176, 1.0)
